1e239f2efebc0263d663b9825fb453eed35a46f5
Hon Chau <chaugeorge9@gmail.com>
2025-03-12 09:54:55 -0500

    updated file path

--- server/src/config/connection.js
+++ server/src/config/connection.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/NBA');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/finalproject');
 
 
 export default mongoose.connection;
