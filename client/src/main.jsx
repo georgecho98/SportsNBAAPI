73babcd7d570cd2cb76afb0d0ca6b74341200463
Hon Chau <chaugeorge9@gmail.com>
2025-03-12 09:26:46 -0500

    updated the package.json

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import SearchSport from './pages/SearchSport.jsx';
 import SavedScoreResult from './pages/SavedScoreResult.jsx';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx';
 
 import NotFound from './pages/NotFound';
